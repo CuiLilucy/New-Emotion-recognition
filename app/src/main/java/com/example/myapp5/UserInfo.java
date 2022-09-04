99a4fbf414224a9eb798655ccf5d9052685ae774
CuiLilucy <cuililucy@163.com>
2022-09-04 11:17:01 +0800

    Initial commit

---
+++ app/src/main/java/com/example/myapp5/UserInfo.java
@@ -0,0 +1,25 @@
+package com.example.myapp5;
+
+public class UserInfo {
+    public long rowid;
+    public int xuhao;
+    public int month;
+    public String date;
+    public String title;
+    public String create_time;
+    public String update_time;
+    public String text;
+
+
+
+    public UserInfo() {
+        rowid =0L;
+        xuhao = 0;
+        date = "";
+        month = 0;
+        title = "";
+        text = "";
+        create_time = "";
+        update_time = "";
+    }
+}
